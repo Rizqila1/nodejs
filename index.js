389f2766a6c821ac822131f31e2eefe6b65cf69c
Rizqila1 <ikilatr@gmail.com>
2023-03-21 21:33:52 +0700

    task express JS

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
-const {hello, world} = require('./module');
-const http = require("http");
-const moment = require('moment/moment');
+// const {hello, world} = require('./module');
+// const http = require("http"); ROUTING NODE JS
+// const moment = require('moment/moment'); // ROUTING MURNI NODE JS
+// const formidable = require('express-formidable');
+// const multer  = require('multer')
+// const upload = multer({dest: 'uploads'})
+const express = require('express');
+const router = require('./router');
 const PORT = 2000
 
-const server = http.createServer((req, res)=> {
-    res.statusCode = 200
-    res.setHeader('Content-type', 'text/json');
-    res.write(JSON.stringify ({
-        status: "Successfully",
-        message: "This is the usage from Node Backend",
-        study: "Node JS",
-        loginAt: moment()
-    }));
-    res.end();
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(router);
+
+// SET NOT FOUND
+app.use((req, res, next) => {
+    res.status(404);
+    res.send({
+        status: 'Error',
+        message: `404 ${req.originalUrl} is Not Found`,
+    });
 });
 
-server.listen(PORT, () => console.log('Server running at localhost:2000'));
+app.listen(PORT, () => console.log('Server running at localhost:2000'));
 
-console.log(hello(), world());
+
+// upload file using Multer library
+// app.post('/cover', upload.single('image'), function (req, res, next) {          
+//     const {name, age, address, gender} = req.body;
+//     const image = req.file;
+
+//     res.json({name, age, address, gender, image});
+// });
+
+
+
+
+// console.log(hello(), world());
+
+// DIBAWAH INI ADALAH CARA ROUTING MURNI MENGGUNAKAN NODE JS
+// const server = http.createServer((req, res)=> {
+//     switch (req.url) {
+//         case '/data': data(res); break;
+//         case '/' : home(res); break;
+//         default: page404(res); break;
+//     }
+// });
+
+// const data = res => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-type', 'text/json');
+//     res.write(JSON.stringify ({
+//         status: "Successfully",
+//         message: "This is the usage from Node Backend",
+//         study: "Node JS",
+//         loginAt: moment()
+//     }));
+//     res.end();
+// }
+
+// const home = res => {
+//     res.statusCode = 200;
+//     res.end('<h1>HOME PAGE</h1>');
+// }
+
+// const page404 = res => {
+//     res.statusCode = 404;
+//     res.end('<h1>Not Found</h1>');
+// }
+
+// server.listen(PORT, () => console.log('Server running at localhost:2000'));
--- module/index.js
+++ module/index.js
@@ -1,14 +1,14 @@
-const hello = () => {
-    return "Hello";
-}
+// const hello = () => {
+//     return "Hello";
+// }
 
-const world = () => {
-    return "World";
-}
+// const world = () => {
+//     return "World";
+// }
 
-console.log(hello(), "ini dari module");
+// console.log(hello(), "ini dari module");
 
-module.exports = {
-    hello,
-    world
-}
+// module.exports = {
+//     hello,
+//     world
+// }
